98b363be19f29e2d59f2c7ebf3bc745bf61bb69c
Shamarin88 <shamarin77@gmail.com>
2022-12-23 14:03:07 +0300

    ДЗ часть 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -45,5 +46,50 @@ public static void main(String[]args) {
     }
     integerSet.removeIf(integer -> integer % 2 != 0);
     System.out.println(integerSet);
+    System.out.println();
+    System.out.println("Задание 3");
+    System.out.println();
+
+    int [][] mas = new int[15][3];
+    for (int i = 0; i < mas.length; i++) {
+        for (int h = 0; h < mas[i].length - 1; h++) {
+            mas[i][h] = (int)(Math.random() * 8) + 2;
+            if (h == mas[i].length - 1) {
+                mas[i][h] = mas[i][h - 2] * mas[i][h - 1];
+                for (int q = 0; q < 1; q++) {
+                    if (mas[i][h] == mas[q][h]) {
+                        if (mas[i][h - 1] == mas[q][h - 1] || mas[i][h - 2] == mas[q][h - 2]) --i;
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < mas.length; i++) {
+        System.out.println("Задание № " + (i + 1 + " Умножить "));
+        for (int h = 0; h < mas[i].length; h++) {
+            if (h == 0)
+                System.out.println(mas[i][h] + " на ");
+            if (h == 1)
+                System.out.println(mas[i][h]);
+        }
+    }
+
+    System.out.println("Задание 4");
+    System.out.println();
+
+    Passport passportOne = new Passport(345987, "Вовк","Дмитрий","Вадимович","02.11.1987");
+    Passport passportTwo = new Passport(156097, "Кулага","Владислав","Александрович","22.08.1992");
+    Passport passportThree = new Passport(874018, "Сытников","Никита","Александрович","13.09.1996");
+    Passport passportFour = new Passport(671045, "Смирнов","Федор","Николаевич","06.09.1985");
+
+    HashMap<Long, Passport> passportMap = new HashMap<>();
+
+    passportMap.put(passportOne.getNumPassport(), passportOne);
+    passportMap.put(passportTwo.getNumPassport(), passportTwo);
+    passportMap.put(passportThree.getNumPassport(), passportThree);
+    passportMap.put(passportFour.getNumPassport(), passportFour);
+    System.out.println(passportMap);
+    System.out.println();
+    System.out.println(passportMap.get(345987));
     }
 }
